5183294eed10b6edbed8e26ff53fff7292055ca1
leejaehee0807 <leejaehee0807@naver.com>
2024-08-28 11:36:48 +0900

    first init

---
+++ src/main/java/com/sparta/springexpert/SpringExpertApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springexpert/SpringExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
