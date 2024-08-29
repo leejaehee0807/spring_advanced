6153eb0131f8f800638e1152c6230fb5fdf5e2e4
leejaehee0807 <leejaehee0807@naver.com>
2024-08-29 10:45:57 +0900

    요구사항 5단계 1번까지 수정

--- src/main/java/com/sparta/springexpert/controller/UserController.java
+++ src/main/java/com/sparta/springexpert/controller/UserController.java
@@ -6,6 +6,8 @@ import com.sparta.springexpert.dto.user.response.UserSaveResponseDto;
 import com.sparta.springexpert.dto.user.response.UserSimpleResponseDto;
 import com.sparta.springexpert.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +19,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public UserSaveResponseDto saveUser(@RequestBody UserSaveRequestDto userSaveRequestDto){
-        return userService.saveUser(userSaveRequestDto);
+    public ResponseEntity<UserSaveResponseDto> saveUser(@RequestBody UserSaveRequestDto userSaveRequestDto){
+        return ResponseEntity.ok(userService.saveUser(userSaveRequestDto));
     }
 
     @GetMapping("/users")
-    public List<UserSimpleResponseDto> getUsers(){
-        return userService.getUsers();
+    public ResponseEntity<List<UserSimpleResponseDto>> getUsers(){
+        return ResponseEntity.ok(userService.getUsers());
     }
 
     @GetMapping("/users/{userId}")
-    public UserDetailResponseDto getUser(@PathVariable Long userId){
-        return userService.getUser(userId);
+    public ResponseEntity<UserDetailResponseDto> getUser(@PathVariable Long userId){
+        return ResponseEntity.ok(userService.getUser(userId));
     }
     @DeleteMapping("/users/{userId}")
     public void deleteUser(@PathVariable Long userId){
